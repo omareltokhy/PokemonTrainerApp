ec30d3feeb46955de351551e82a2b1597d438030
Mikko Nikku <mikko.nikku@fi.experis.com>
2021-09-30 15:03:49 +0300

    typo

--- src/app/Services/auth.guard.ts
+++ src/app/Services/auth.guard.ts
@@ -15,8 +15,6 @@ export class AuthGuard implements CanActivate {
 
   }
   
-
-  
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
