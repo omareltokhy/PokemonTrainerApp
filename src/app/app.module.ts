4ce788c707dbc7cea9a0d70d77bf782158f7f2c5
Omar El Tokhy <omar.el.tokhy@fi.experis.com>
2021-10-03 18:47:59 +0300

    fix formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
 import { TrainerComponent } from './trainer/trainer.component';
 import { AppRoutingModule } from './app-routing.module';
-
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 
